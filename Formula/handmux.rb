36b9cae6695296311cd546b74d3f5cb081bf13ad
Louie Jin <yuanyuanzijin@gmail.com>
2026-08-09 12:14:09 +0800

    handmux 0.23.0

--- Formula/handmux.rb
+++ Formula/handmux.rb
@@ -1,8 +1,8 @@
 class Handmux < Formula
   desc "Mobile-web gateway to a shared tmux server"
   homepage "https://handmux.com"
-  url "https://registry.npmjs.org/handmux/-/handmux-0.22.1.tgz"
-  sha256 "0013849358af4c89c7e9fcb85c698bad5d1361ad252010b76899829bd79b8ac7"
+  url "https://registry.npmjs.org/handmux/-/handmux-0.23.0.tgz"
+  sha256 "fd2fa1a992f8e65b8513c6cc6896d0f20c5c2ea5b96f19b651212f66bc186521"
   license "AGPL-3.0-only"
 
   livecheck do
